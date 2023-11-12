9bc22172d9ffa620b8c2da9968cbc291471f96d4
almono <pwalczykiewicz93@gmail.com>
2023-11-12 18:53:47 +0100

    Gem pickups and music/sfx

---
+++ Assets/Scripts/Collectibles/Gem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            //Destroy(gameObject);
+
+            // More performance heavy
+            GetComponent<AudioSource>().Play();
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<CircleCollider2D>().enabled = false;
+        }
+    }
+}
